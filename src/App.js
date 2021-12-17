300884add387bba4d380c096032f54a0fbd5d9f8
zafar <zafar@mudah.my>
2021-12-17 14:14:08 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          This is for my blog post tutorial on setting up github actions with heroku for CI/CD.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
