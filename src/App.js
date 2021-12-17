ad45dba3c24e16f882abdec90cc84b1e84dc6216
zafar <zafar@mudah.my>
2021-12-17 14:40:56 +0500

    feat: Text updated

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      	This is for my 
         <a
           className="App-link"
           href="https://github.com/zafar-saleem/github-actions-heroku"
           target="_blank"
           rel="noopener noreferrer"
         >
-          This is for my blog post tutorial on setting up github actions with heroku for CI/CD.
+          blog post tutorial
         </a>
+         on setting up github actions with heroku for CI/CD.
         <button>Does nothing</button>
       </header>
     </div>
